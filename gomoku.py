7ef085a4e8e994b332fdf378d465a14069cb2b0c
Khoa Nguyen <nguyenk7@miamioh.edu>
2020-12-19 15:15:16 -0500

    Button change

--- gomoku.py
+++ gomoku.py
@@ -2,7 +2,8 @@ from math import pi
 import math
 import random
 import pygame
-
+import sys
+from pygame.locals import *
 
 # Define variables at global scope first before using them
 x_margin = None
@@ -81,7 +82,6 @@ class Board(object):
                 else:
                     text = font.render('o', True, (0, 0, 0))
                 surface.blit(text, (k * 40 + 20, l * 40))
-
 
 class ultility:
     @staticmethod
@@ -197,7 +197,6 @@ def startBoard():
     iniStatus = [[0 for x in range(COL)] for y in range(ROW)]
     key = Board(iniStatus, 0, COL, ROW, ai, createPatternDict())
 
-
 def reDraw(surface):
     surface.fill((0, 0, 0))
     key.draw(surface)
@@ -247,9 +246,78 @@ def main():
     # main loop
     flag = True
     while flag:
+        button = pygame.Rect(50, 100, 100, 50)
+        pygame.draw.rect(frame, (255, 0, 0), button)
         key.listen()
         reDraw(frame)
         if ultility.checkWin(key.value):
             pygame.time.delay(50)
             startBoard()
-main()
+
+mainClock = pygame.time.Clock()
+pygame.init()
+pygame.display.set_caption('game base')
+screen = pygame.display.set_mode((500, 500),0,32)
+ 
+font = pygame.font.SysFont(None, 20)
+ 
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+ 
+click = False
+ 
+def main_menu():
+    while True:
+        screen.fill((0,0,0))
+        draw_text('main menu', font, (255, 255, 255), screen, 20, 20)
+ 
+        mx, my = pygame.mouse.get_pos()
+ 
+        button_1 = pygame.Rect(50, 100, 200, 50)
+        button_2 = pygame.Rect(50, 200, 200, 50)
+        if button_1.collidepoint((mx, my)):
+            if click:
+                main()
+        if button_2.collidepoint((mx, my)):
+            if click:
+                options()
+        pygame.draw.rect(screen, (255, 0, 0), button_1)
+        pygame.draw.rect(screen, (255, 0, 0), button_2)
+ 
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+ 
+        pygame.display.update()
+        mainClock.tick(60)
+ 
+def options():
+    running = True
+    while running:
+        screen.fill((0,0,0))
+ 
+        draw_text('options', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+        
+        pygame.display.update()
+        mainClock.tick(60)
+ 
+main_menu()
