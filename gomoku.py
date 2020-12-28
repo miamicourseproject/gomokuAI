da844f774e5101185e51de2ebeae3bdb755190c4
Khoa Nguyen <nguyenk7@miamioh.edu>
2020-12-27 20:26:51 -0500

    minor commit

--- gomoku.py
+++ gomoku.py
@@ -148,6 +148,7 @@ def main(AITurn, size = 15):
                 key.listen(pos)
                 screen.fill(black)
                 backButton.draw(screen, white)
+                hintButton.draw(screen, white)
                 key.draw(screen)
                 pygame.display.update()
 
